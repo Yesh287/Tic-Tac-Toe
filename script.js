783c72088fdd0d3d4599c75985bcfd045cf466ea
Yesh287 <yashagarwal287@gmail.com>
2025-06-12 14:56:04 +0530

    Function for a Draw

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let boxes = document.querySelectorAll(".btn");
 let reset = document.querySelector("#reset"); 
 let newGame = document.querySelector("#newGame"); 
 let turn = true; // 1st player's turn
-let Result = document.querySelector(".result");
+let result = document.querySelector(".result");
 let announcement = document.querySelector("#winner");
 let turnAnnoucement = document.querySelector("#turnAnnoucement");
 console.log(turnAnnoucement);
@@ -15,6 +15,7 @@ const winPatterns = [
     [3,4,5],
     [6,7,8],
 ];
+let count = 0;
 boxes.forEach((box) => {
     box.addEventListener("click",()=>{
         if(turn){
@@ -25,38 +26,55 @@ boxes.forEach((box) => {
             box.innerHTML = "O";
             turnAnnoucement.innerText = "X's Turn";
         }
+        count++;
         turn = !turn;
         box.disabled = true;
         checkWinner();
     })
 });
 const checkWinner = () =>{
+    let noWinner = true;
     for(let pattern of winPatterns){
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
         let pos3 = boxes[pattern[2]].innerText;
         if(pos1 === pos2 && pos2 === pos3 && pos1 != ""){
+            noWinner = false;
             let winner = turn?'O':'X';
             declareWinner(winner);
         }
     }
+    if(count === 9 && noWinner){
+        declareDraw();
+    }
 }
 const declareWinner = (winner) => {
-    Result.classList.remove("hide");
+    count = 0;
     announcement.innerText = `Congratulations! Winner is ${winner}`;
+    result.classList.remove("hide");
+    for(let box of boxes){
+        box.disabled = true;
+    }
+    window.scrollTo({top:0,behavior:"instant"});
+}
+const declareDraw = () => {
+    count = 0;
+    announcement.innerText = `Draw!`;
+    result.classList.remove("hide");
     for(let box of boxes){
         box.disabled = true;
     }
     window.scrollTo({top:0,behavior:"instant"});
 }
 const resetGame = () =>{
+    count = 0;
     turn = true;
     for(let box of boxes){
         box.disabled = false;
         box.innerText = "";
     }
     turnAnnoucement.innerText = "X's Turn";
-    Result.classList.add("hide");
+    result.classList.add("hide");
 }
 reset.addEventListener("click",resetGame);
 newGame.addEventListener("click",resetGame);
